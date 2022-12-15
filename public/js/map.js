5533ee2272c1e08b7082a5bc691a6a8774eadc69
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-12-15 15:07:12 +0100

    adding map

---
+++ public/js/map.js
@@ -0,0 +1,6 @@
+const map = new maplibregl.Map({
+    container: "map",
+    style: "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
+    center: [2.3522, 48.8566],
+    zoom: 5
+});
