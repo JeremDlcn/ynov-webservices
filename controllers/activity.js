4e5c2fe754335585aea1a41d635928f20bced4bc
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2023-01-16 19:40:12 +0100

    change temporarily the name

--- controllers/activity.js
+++ controllers/activity.js
@@ -1,4 +1,4 @@
-const Activity = require('../models/Activity');
+const Activity = require('../models/Activitys');
 const climbing = require('./climbing');
 const hiking = require('./hiking');
 
@@ -20,7 +20,7 @@ exports.getOneActivityByCategory = (req, res, next) => {
         .catch(error => res.status(404).json({ error }));
 }
 
-exports.getActivitiesByCategory = async (req, res, next) => {
+exports.getActivitiesByCategory = async(req, res, next) => {
     if (req.params.category === 'climbing') {
         const response = await climbing.getClimbingDatas([43.28403200089498, 5.371308351623643]);
         res.status(200).json(response);
@@ -36,6 +36,20 @@ exports.getActivitiesByCategory = async (req, res, next) => {
 
 exports.createActivity = (req, res, next) => {
     Activity.create({
+            name: req.body.name,
+            description: req.body.description,
+            schedule: req.body.schedule,
+            coords: req.body.coords,
+            category: req.body.category,
+            prices: req.body.prices,
+            icon: req.body.icon
+        })
+        .then(() => res.status(201).json({ message: 'Activité créée' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.modifyActivity = (req, res) => {
+    Activity.updateOne({ _id: req.params.id }, {
         name: req.body.name,
         description: req.body.description,
         schedule: req.body.schedule,
@@ -44,25 +58,10 @@ exports.createActivity = (req, res, next) => {
         prices: req.body.prices,
         icon: req.body.icon
     })
-    .then(() => res.status(201).json({message: 'Activité créée'}))
-    .catch(error => res.status(400).json({ error }));
-}
-
-exports.modifyActivity = (req, res) => {
-    Activity.updateOne({ _id: req.params.id },
-    {
-        name: req.body.name,
-        description: req.body.description,
-        schedule: req.body.schedule,
-        coords: req.body.coords,
-        category: req.body.category,
-        prices: req.body.prices,
-        icon: req.body.icon
-    })  
 }
 
 exports.deleteActivity = (req, res) => {
     Activity.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({message: 'Activité supprimée'}))
-    .catch(error => res.status(400).json({ error }));
+        .then(() => res.status(200).json({ message: 'Activité supprimée' }))
+        .catch(error => res.status(400).json({ error }));
 }
